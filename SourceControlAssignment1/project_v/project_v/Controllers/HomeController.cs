8db34077915c6fe3a70958e4429bbc0a04b33d06
Mmarlin <40515686+Mmarlin@users.noreply.github.com>
2020-12-01 22:07:48 +0530

    Source Control Assignment
    This is submission of Source Control Assignment 1

---
+++ SourceControlAssignment1/project_v/project_v/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+using project_v.Models;
+namespace project_v.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult StudentDetails(StudentModel sm)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.name = sm.Name;
+                ViewBag.email = sm.Email;
+                ViewBag.age = sm.Age;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.name = "No Data";
+                ViewBag.email = "No Data";
+                ViewBag.age = "No Data";
+                return View("Index");
+            }
+        }
+    }
+}
---
+++ SourceControlAssignment1/project_v/project_v/Models/StudentModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Web.Mvc;
+
+namespace project_v.Models
+{
+    [Bind(Exclude = "Id")]
+    public class StudentModel
+    {
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+       
+        [Required(ErrorMessage = "Name is Required")]
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email ID is Required")]
+        [DataType(DataType.EmailAddress)]
+        [MaxLength(50)]
+        [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Incorrect Email Format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Confirm Email is Required")]
+        [DataType(DataType.EmailAddress)]
+        [System.ComponentModel.DataAnnotations.Compare("Email", ErrorMessage = "Email Not Matched")]
+        public string ConfirmEmail { get; set; }
+
+        [Required(ErrorMessage = "Age is Required")]
+        [Range(1, 120, ErrorMessage = "Age must be between 1-120 in years.")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{4})?([0-9]{3})?([0-9]{3})$", ErrorMessage = "Not a valid Phone number")]
+        public string PhoneNumber { get; set; }
+
+        
+        [Url]
+        [Required (ErrorMessage ="Your Website is required")]
+        public string URL { get; set; }
+    }
+}
